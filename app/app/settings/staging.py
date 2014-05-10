b3873f92ef3e8d409d6d8e906dc301a0c8406784
dingledow <david@ingledow.co.uk>
2014-05-10 15:47:05 +0100

    Update to static and media s3

--- app/app/settings/staging.py
+++ app/app/settings/staging.py
@@ -5,15 +5,15 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 AWS_STORAGE_BUCKET_NAME = 'ingledow-staging'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'app.settings.s3utils.StaticRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'app.settings.s3utils.MediaRootS3BotoStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../../srv/assets'),
 )
 
-STATIC_ROOT = 'http://ingledow-staging.s3.amazonaws.com/'
-STATIC_URL = 'http://ingledow-staging.s3.amazonaws.com/'
+STATIC_ROOT = 'http://ingledow-staging.s3.amazonaws.com/static/'
+STATIC_URL = 'http://ingledow-staging.s3.amazonaws.com/static/'
 
-MEDIA_ROOT = 'http://ingledow-media.s3.amazonaws.com/'
-MEDIA_URL = 'http://ingledow-media.s3.amazonaws.com/'
+MEDIA_ROOT = 'http://ingledow-staging.s3.amazonaws.com/media/'
+MEDIA_URL = 'http://ingledow-staging.s3.amazonaws.com/media/'
